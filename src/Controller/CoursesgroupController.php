03f91026cb9402c4ec67a5342d3cdff7e7d4c513
btlam87 <btlam87@gmail.com>
2020-04-24 20:47:59 +0700

    khuya 24/04/2020

---
+++ src/Controller/CoursesgroupController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Coursegroup;
+use App\Entity\Course;
+
+class CoursesgroupController extends AbstractController
+{
+    /**
+     * @Route("/coursesgroup", name="coursesgroup")
+     */
+    public function coursegroup()
+    {
+        $coursesgroup = $this->getDoctrine()->getRepository(Coursegroup::class)->findAll();
+        $course = $this->getDoctrine()->getRepository(Course::class)->findAll();
+        return $this->render('coursesgroup/index.html.twig', [
+            'title' => 'Các khóa học tại trung tâm ngoại ngữ Minh Anh',
+            'coursesgroup_list'=>$coursesgroup,
+            'course_list' =>$course,
+        ]);
+    }
+}
--- src/Entity/Coursegroup.php
+++ src/Entity/Coursegroup.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CoursegroupRepository")
+ * @Vich\Uploadable()
  */
 class Coursegroup
 {
@@ -20,6 +22,17 @@ class Coursegroup
      * @ORM\Column(type="string", length=255)
      */
     private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $thumb;
+
+    /**
+     * @Vich\UploadableField(mapping = "coursesgroupthumb", fileNameProperty="thumb")
+     */
+    private $thumbFile;
+
 
     public function getId(): ?int
     {
@@ -40,5 +53,28 @@ class Coursegroup
     public function __Tostring()
     {
         return $this->name;
+    }
+
+    public function getThumb(): ?string
+    {
+        return $this->thumb;
+    }
+
+    public function setThumb(?string $thumb): self
+    {
+        $this->thumb = $thumb;
+
+        return $this;
+    }
+
+    public function getThumbFile()
+    {
+        return $this->thumbFile;
+    }
+
+    public function setThumbFile($thumbFile): void
+    {
+        $this->thumbFile = $thumbFile;
+       
     }
 }
