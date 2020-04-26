4b408072593dc494de512f2e17d360d66023965c
btlam87 <btlam87@gmail.com>
2020-04-26 16:40:41 +0700

    xong admin slider

---
+++ src/Entity/Slide.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints\DateTime;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SlideRepository")
+ * @Vich\Uploadable()
+ */
+class Slide
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Thumb;
+    /**
+     * @Vich\UploadableField(mapping = "sliderthumb", fileNameProperty="thumb")
+     */
+    private $thumbFile;
+
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $link;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $createdate;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $status;
+
+    public function __construct()
+    {
+        $this->createdate = new \DateTime();
+        $this->status = true;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getThumb(): ?string
+    {
+        return $this->Thumb;
+    }
+
+    public function setThumb(string $Thumb): self
+    {
+        $this->Thumb = $Thumb;
+
+        return $this;
+    }
+    public function getThumbFile()
+    {
+        return $this->thumbFile;
+    }
+
+    public function setThumbFile($thumbFile): void
+    {
+        $this->thumbFile = $thumbFile;
+   
+    }
+
+    public function getLink(): ?string
+    {
+        return $this->link;
+    }
+
+    public function setLink(?string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedate(): ?\DateTimeInterface
+    {
+        return $this->createdate;
+    }
+
+    public function setCreatedate(?\DateTimeInterface $createdate): self
+    {
+        $this->createdate = $createdate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
